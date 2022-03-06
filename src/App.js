fc496ea5b65018961a418fe28a7a3935230e7078
YBogdan RADOI <radoi.office@gmail.com>
2022-03-06 20:50:23 +0100

    Migrate v6 to v5 Router + Route Params

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { Route } from "react-router-dom";
 
-import MainHeader from './components/MainHeader';
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
+import MainHeader from "./components/MainHeader";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
-    <Router>
+    <div>
       <MainHeader />
-     <Routes>
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/products" element={<Products />} />
-     </Routes>
-    </Router>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+      <Route path="/product-detail/:productId">
+        <ProductDetail />
+      </Route>
+    </div>
   );
 }
 
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,12 @@
 const Products = () => {
-  return <h1>The Products Page</h1>;
+  return <section>
+    <h1>The Products Page</h1>
+    <ul>
+      <li>A book</li>
+      <li>A carpet</li>
+      <li>An online course</li>
+    </ul>
+  </section>;
 };
 
 export default Products;
