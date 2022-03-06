8e919e48566b7693049b6b64ea3ef7c8bf757815
YBogdan RADOI <radoi.office@gmail.com>
2022-03-06 12:04:49 +0100

    Links, Route & Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import MainHeader from './components/MainHeader';
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Router>
+      <MainHeader />
+     <Routes>
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/products" element={<Products />} />
+     </Routes>
+    </Router>
   );
 }
 
